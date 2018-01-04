f24fc12dc8a886d16e1ebf6e00ab9585973152c1
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2018-01-05 04:21:00 +0900

    implement REST client

--- rest-client/project.clj
+++ rest-client/project.clj
@@ -3,7 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[cheshire "5.8.0"]
+                 [clj-http "3.7.0"]
+                 [org.clojure/clojure "1.9.0"]]
   :main ^:skip-aot rest-client.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
