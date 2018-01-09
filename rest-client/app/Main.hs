e8fbd9e4cb65d9633e9f7d51ce2a246f528cd55c
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2018-01-09 21:25:43 +0900

    implement REST client

--- rest-client/app/Main.hs
+++ rest-client/app/Main.hs
@@ -1,6 +1,41 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import Lib
+import           Control.Monad              (when)
+import           Data.Char                  (toUpper)
+import           System.Environment         (getArgs)
+
+import           Control.Lens               ((&))
+import           Data.Aeson                 (Value, eitherDecode)
+import           Data.Aeson.Text            (encodeToLazyText)
+import qualified Data.ByteString.Lazy.Char8 as BLC
+import qualified Data.Text.Lazy.IO          as L
+import           Network.HTTP.Simple
 
 main :: IO ()
-main = someFunc
+main = do
+    args <- getArgs
+    when (length args /= 2) $ error "Exactly 2 arguments must be specified"
+    let [url, method] = args
+    res <- http url method
+    let status = getResponseStatusCode res
+    when (status /= 200) . putStr $ show status ++ "\t"
+    L.putStrLn . encodeToLazyText $ getResponseBody res
+
+http :: String -> String -> IO (Response Value)
+http url method = do
+    req <- parseRequest url
+    case map toUpper method of
+        "GET" -> httpJSON $ req & setRequestMethod "GET"
+                                & setRequestHeader "Accept" ["application/json"]
+        "POST" -> do
+            json <- readJSONfromStdin
+            httpJSON $ req & setRequestMethod "POST"
+                           & setRequestBodyJSON json
+                           & setRequestHeader "Accept" ["application/json"]
+        _ -> error $ "Unsupported HTTP method: " ++ method
+
+readJSONfromStdin :: IO Value
+readJSONfromStdin = do
+    input <- BLC.getContents
+    return . either error id $ eitherDecode input
